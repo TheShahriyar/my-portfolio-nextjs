bc26646ab48b298f70f44f61042de6f1324c0133
TheShahriyar <shahriyar.31dec@gmail.com>
2023-07-23 16:13:59 +0600

    api delete from  project

--- src/components/useContentful.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require("contentful");
-
-const client = createClient({
-  space: "soc6vx8zddf8",
-  accessToken: "0ABhDEtkGYJ1W8kqDjlGZvPLOBs8iPmvY--ezifK5H0",
-});
-
-export default client;
